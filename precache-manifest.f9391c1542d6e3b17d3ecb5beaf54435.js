d347feec2318ff55d7f425d3f456832fb6f41c00
ziratsu <enzo.ustariz@hotmail.fr>
2019-05-07 12:19:48 +0200

    Updates

---
+++ precache-manifest.f9391c1542d6e3b17d3ecb5beaf54435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efed38152f35eedd089a99ddac27a872",
+    "url": "/appreact/index.html"
+  },
+  {
+    "revision": "ee81168e3d2c087d7cc2",
+    "url": "/appreact/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/appreact/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ee81168e3d2c087d7cc2",
+    "url": "/appreact/static/js/main.ddb4d14a.chunk.js"
+  },
+  {
+    "revision": "46c9aefe9a753a431924",
+    "url": "/appreact/static/js/runtime~main.ac1b2f02.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/appreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
